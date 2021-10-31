6c0513a97d796db4a49d97fee785f91cec62a3be
ARunkumar <greenarun@yahoo.com>
2021-10-31 23:23:33 +0800

    modified for iframe

--- src/modules/HomeBody/HomeBody.component.js
+++ src/modules/HomeBody/HomeBody.component.js
@@ -118,7 +118,7 @@ const HomeBody = () => {
                         <Typography variant="button" display="block" gutterBottom>
                             Facebook
                         </Typography>
-                        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FIndianPeopleSocialServicework%2F&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" width="340" height="500" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                        <iframe title="facebook" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FIndianPeopleSocialServicework%2F&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" width="340" height="500" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
                     </Box>
                     <Divider />
 
